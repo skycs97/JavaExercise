52518336d299f6db12f735a272ffd9fa6439fba3
skycs97 <skycs97@naver.com>
2016-03-29 13:49:46 +0900

    Update Rectangle.java

--- src/classes/Rectangle.java
+++ src/classes/Rectangle.java
@@ -1,43 +1,63 @@
-package classes;
-
+import java.util.Scanner;
 public class Rectangle {
     private int x1, y1, x2, y2;
 
     public void setPoints(int _x1, int _y1, int _x2, int _y2) {
-        // TODO, check if x, y > 0
-        x1 = _x1;
-        y1 = _y1;
-        x2 = _x2;
-        y2 = _y2;
+       if(_x1<=0 || _y1<=0 || _x2<=0 || _y2<=0) {
+           System.out.println("다시 입력하시오.");
+           Scanner input = new Scanner(System.in);
+           x1 = input.nextInt();
+           y1 = input.nextInt();
+           x2 = input.nextInt();
+           y2 = input.nextInt();
+       }
+        if(_x1>_x2) {
+            x1 = _x2;
+            x2 = _x1;
+        }
+        else if(_x1<_x2){
+            x1 = _x1;
+            x2 = _x2;
+        }
+        if(_y1>_y2) {
+            y1 = _y2;
+            y2 = _y1;
+        }
+        else if(_y1<_y2){
+            y1 = _y1;
+            y2 = _y2;
+        }
     }
 
     public int getWidth() {
-        // TODO
-        return 0;
+        return (x2-x1);
     }
 
     public int getHeight() {
-        // TODO
-        return 0;
+        return (y2-y1);
     }
 
     public int getArea() {
-        // TODO
-        return 0;
+        int Area = 0;
+        Area = (x2-x1) * (y2-y1);
+        return Area;
     }
 
     public boolean equals(Rectangle r) {
-        // TODO
+        if((x1==r.x1) && (x2==r.x2) && (y1==r.y1) && (y2==r.y2))
+            return true;
         return false;
     }
 
     public boolean in(Rectangle r) {
-        // TODO
+        if((r.x1<=x1&&x1<=r.x2) && (r.x1<=x2&&x2<=r.x2) && (r.y1<=y1&&y1<=r.y2) && (r.y1<=y2&&y2<=r.y2))
+            return true;
         return false;
     }
 
     public boolean overlap(Rectangle r) {
-        // TODO
+        if(((x1<=r.x1&&r.x1<=x2) || (x1<=r.x2&&r.x2<=x2)) && ((y1<=r.y1&&r.y1<=y2) || (y1<=r.y2&&r.y2<=y2)))
+            return true;
         return false;
     }
 
