caf88db90b94cc6980d3070306b4895104f3540e
skycs97 <skycs97@naver.com>
2016-04-05 13:57:44 +0900

    Update Lake10.java

--- src/classes/Lake10.java
+++ src/classes/Lake10.java
@@ -1,10 +1,47 @@
-package classes;
-
+/**
+ * Created by 철순 on 2016-04-05.
+ */
 import java.util.Scanner;
-
 /**
  * Created by jyheo on 2016-04-04.
  */
+class Fish {
+    private String name;
+    private String shape;
+    protected int x, y;
+
+    public Fish(String name, String shape) {
+        this.name = name;
+        this.shape = shape;
+        x = 0;
+        y = 0;
+    }
+
+    public Fish() {
+        this.name = "Unknown";
+        this.shape = "<--<";
+        x = 0;
+        y = 0;
+    }
+
+    public void move(int width, int height) {
+        double rand = Math.random();
+        if (rand < 0.5)
+            x++;
+        else
+            y++;
+        if (x >= width)
+            x = 0;
+        if (y >= height)
+            y = 0;
+    }
+
+    public void display(int x, int y) {
+        if (this.x == x && this.y == y) {
+            System.out.print(shape);
+        }
+    }
+}
 class FoolFish extends Fish {
     FoolFish(String name, String shape) {
         super(name, shape);
@@ -19,7 +56,6 @@ class FoolFish extends Fish {
             y = 0;
     }
 }
-
 public class Lake10 {
     private int width;
     private int height;
